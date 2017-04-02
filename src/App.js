bf07c28a311de70edbbbc959753ab23c151b9f86
bhuvan malik <bhuvanmalik007@gmail.com>
2017-04-02 23:46:55 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -2,33 +2,38 @@ import React, {Component} from 'react';
 import Header from './components/header';
 import SearchBar from './components/searchbar';
 import GifGrid from './components/grid';
+import {Loader} from 'semantic-ui-react';
 import './App.css';
-// let gifs = [];
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      gifs: []
+      gifs: [],
+      loading: false
     };
+    this.searchGiphy = this.searchGiphy.bind(this);
   }
 
-  searchGiphy = (event) =>{
+  searchGiphy(event) {
     var that = this;
     if (event.key === 'Enter') {
-      fetch('http://api.giphy.com/v1/gifs/search?q=' + event.target.value + '&api_key=dc6zaTOxFJmzC', {method: 'get'})
-      .then(res => res.json())
-      .then(result => that.setState({gifs: result.data}));
+      this.setState({loading:true});
+      fetch('http://api.giphy.com/v1/gifs/search?q=' + event.target.value + '&api_key=dc6zaTOxFJmzC', {method: 'get'}).then(res => res.json()).then(result => that.setState({gifs: result.data, loading:false}));
     }
   }
 
   render() {
+
     return (
       <div className="App">
         <Header/>
         <SearchBar searchGiphy={this.searchGiphy}/>
-        <br/>
-        <GifGrid gifs={this.state.gifs} className="Grid"/>
+        <br/> {this.state.loading
+          ? (
+            <Loader size='massive'>Loading</Loader>
+          )
+          : (<GifGrid gifs={this.state.gifs} className="Grid"/>)}
       </div>
     );
   }
